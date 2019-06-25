f128834d6d778cdd6230cff2c93668353b318d86
Deepak Singh <41149235+sdeepak04@users.noreply.github.com>
2019-06-25 13:14:19 +0530

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import wolframalpha as w
+
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service" + format(e))
+
+    Client = w.Client('LU9QW3-3JQ2PA6JRT')
+
+    query = (text)
+    res = Client.query(query)
+    output = next(res.results).text
+    print(output)
